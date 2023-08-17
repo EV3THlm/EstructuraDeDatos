69764cf8c3438e3a5905adbc8a4d92544bdc33f3
EV3TH <38407419+EV3THlm@users.noreply.github.com>
2023-08-17 10:04:57 -0600

    Practica 3

---
+++ Unidad1/Practicas/P3-BienvenidaBucle.py
@@ -0,0 +1,15 @@
+lim = 1
+i = 0;
+while i <= lim:
+  nombre = str (input ('Ingresa tu nombre: '));
+  edad = int(input('Ingresa tu edad: '));
+  print(f"\nBienvenido!, {nombre}");
+  print(f"\nTienes {edad} años");
+
+  print('\n¿Desea ingresar otros datos?');
+  print('1) Si');
+  print('2) No');
+  lim = int (input('Ingresa tu opcion: '));
+
+  if lim == 2:
+    break
