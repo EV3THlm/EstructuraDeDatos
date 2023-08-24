eef60a1d25a59bd55263d807e10a1e5062198e79
EV3TH <38407419+EV3THlm@users.noreply.github.com>
2023-08-24 10:39:34 -0600

    Practica 6
    Clase 24/Ago/23

---
+++ Unidad1/Practicas/P6-EjemploArray2.py
@@ -0,0 +1,10 @@
+versiones_plone = [2.5, 3.6, 4, 5]
+print(f'{versiones_plone}');
+versiones_plone.append(6); # Agreamos el numero '6' a nuestra lista ya existente
+print(f'{versiones_plone}');
+
+# 2da prueba
+
+versiones_plone.append('Prueba1'); # agregamos un string a la lista existente 
+versiones_plone.append(['Arroz', 'Pollo', 'brocoli', ['2do arreglo', 7863, 'Ebeth']]); # agregamos una lista a la lista ya existente 
+print(f'{versiones_plone}');
